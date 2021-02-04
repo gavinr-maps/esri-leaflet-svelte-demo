95b83db2f4bf12970bfcf4649c9cd073f873a066
Gavin Rehkemper <gavin@gavinr.com>
2021-02-04 00:05:18 -0600

    esri-leaflet-vector basemap

--- src/Map.svelte
+++ src/Map.svelte
@@ -1,14 +1,17 @@
 <script>
   import { Map as LeafletMap, Icon, Marker } from "leaflet";
-  import { VERSION, BasemapLayer, FeatureLayer } from "esri-leaflet";
+  // import { VERSION, BasemapLayer, FeatureLayer } from "esri-leaflet";
+  import { vectorBasemapLayer } from "esri-leaflet-vector";
+
   import "leaflet/dist/leaflet.css";
 
   const map = (domNode) => {
     const map = new LeafletMap(domNode);
     map.setView([53.35014, -6.266155], 8);
 
-    const basemap = new BasemapLayer("Streets");
-    map.addLayer(basemap);
+    vectorBasemapLayer("ArcGIS:Streets", {
+      apiKey: "< YOUR VALID API KEY HERE >", // https://developers.arcgis.com
+    }).addTo(map);
   };
 </script>
 
