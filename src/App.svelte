56e13ccf32083b92ecec0b095d3dd6b645a69f20
Gavin Rehkemper <gavin@gavinr.com>
2021-02-04 00:00:48 -0600

    map working

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,15 @@
 <script>
-	export let name;
+  import Map from "./Map.svelte";
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <Map />
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  main {
+    margin: 0 auto;
+		width: 100%;
+		height: 100%;
+  }
 </style>
---
+++ src/Map.svelte
@@ -0,0 +1,22 @@
+<script>
+  import { Map as LeafletMap, Icon, Marker } from "leaflet";
+  import { VERSION, BasemapLayer, FeatureLayer } from "esri-leaflet";
+  import "leaflet/dist/leaflet.css";
+
+  const map = (domNode) => {
+    const map = new LeafletMap(domNode);
+    map.setView([53.35014, -6.266155], 8);
+
+    const basemap = new BasemapLayer("Streets");
+    map.addLayer(basemap);
+  };
+</script>
+
+<div use:map />
+
+<style>
+  div {
+    height: 100%;
+    width: 100%;
+  }
+</style>
